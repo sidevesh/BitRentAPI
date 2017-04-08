123288652912867adb465f93c1d1a76fc4e75bd2
SiDevesh <sid.swapnildevesh@gmail.com>
2017-04-09 04:44:57 +0530

    Add transaction support

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,3 +1,12 @@
+require 'uri'
+require 'net/http'
+
+class ::Float
+  def encode_json(opts = nil)
+    "%.10f" % self
+  end
+end
+
 module Api
   module V1
   	class ItemsController < ApplicationController
@@ -26,7 +35,10 @@ module Api
           @item.email = nil
           @item.auth_token = nil
           @pastt = @item.updated_at
+          @auth_t = @item.auth_token
+          @sat_amt = (((Time.now.to_datetime - @pastt.to_datetime)*24*60*60).to_i)*@item.tariff
           if @item.save
+            callUno(@auth_t, @sat_amt)
             render json: @item.as_json.merge({:bill => (((Time.now.to_datetime - @pastt.to_datetime)*24*60*60).to_i)*@item.tariff }) and return
           else
             render status: :unprocessable_entity, json: { errors: ["Save failed."] } and return
@@ -50,6 +62,26 @@ module Api
         if @item.auth_token != params[:auth_token]
           render status: :unauthorized, json: { errors: ["Unauthorized."] }
         end
+      end
+
+      def callUno(auth_token, amount)
+        p auth_token
+        p amount
+
+        url = URI("https://sandbox.unocoin.co/api/v1/wallet/sendingbtc")
+
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+        request = Net::HTTP::Post.new(url)
+        request["content-type"] = 'application/json'
+        request["authorization"] = auth_token
+        request["cache-control"] = 'no-cache'
+        request.body = "{\r\n\t\"to_address\":\"3Eudzybvhf7EkEJQyTK42h7CsagQoWhYEn\",\r\n\t\"btcamount\":\""+(amount.to_f/100000000).encode_json+"\"\r\n}"
+
+        response = http.request(request)
+        p response.read_body
       end
 
   	end
